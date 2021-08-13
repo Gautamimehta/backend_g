49e538417bba87629c566032a49a30cff8b0ecee
gautami <gautamimehta17@gmail.com>
2021-08-13 17:22:27 +0530

    initial commit

--- db/connector.js
+++ db/connector.js
@@ -1,12 +1,9 @@
-
-
-
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 const password = process.env.MONGO_PASS;
 mongoose.connect(
-  `mongodb+srv://prakhar:prakhar2306@cluster0.ud330.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+  `mongodb+srv://gautami:gautami23@cluster0.ud330.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
